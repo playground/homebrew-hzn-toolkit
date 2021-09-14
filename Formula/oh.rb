f881212de1d0739fc7d99740089651c2cfa901a1
Jeff Lu <ljeff@us.ibm.com>
2021-09-14 12:27:02 -0400

    update formula

--- Formula/oh.rb
+++ Formula/oh.rb
@@ -2,7 +2,7 @@ class Oh < Formula
   desc "Open Horizon CLI Toolkit"
   homepage "https://github.com/playground/homebrew-hzn-toolkit"
   url "https://github.com/playground/homebrew-hzn-toolkit/releases/download/0.0.1/oh-macos-x64.tar.gz"
-  sha256 "eecedb4c6778180a528a9d565b29a40ff54e181e6d0745c9a0587d36d3f08e43"
+  sha256 "f1698ad0cb33df0477d47abe7e0be347c4106961fb2409cfe73ac99da21fa9e2"
   version "0.0.1"
   def install
     bin.install "oh"
